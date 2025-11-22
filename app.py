71d79dad2baaf22e1168db174fc6fb0ee2517d9e
elmerohueso <elmerohueso@gmail.com>
2025-11-22 11:43:35 -0700

    version bump

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
 
 # Application version
-__version__ = '0.9.8'
+__version__ = '0.9.9'
 # Github repo URL
 GITHUB_REPO_URL = 'https://github.com/elmerohueso/FamilyChores'
 
